5ff80136af04387e561a3a09bcbe64f0339b3820
manomanulinda <162719891+manomanulinda@users.noreply.github.com>
2024-03-12 11:23:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://m.media-amazon.com/images/M/MV5BYzhkNGQ4NDktODZmOC00Y2Y2LWIyZWEtNjkxOTJiNzAxNjU2XkEyXkFqcGdeQXVyNDQxODcxNjU@._V1_.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://i.pinimg.com/736x/d5/9f/18/d59f18e21ff38f2cd2419efd58946f89.jpg";
+}
+
